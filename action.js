6bdfc8d105399d75c6761166edf99b80a7bd26b8
Greg Ziegan <greg.ziegan@gmail.com>
2020-04-07 11:03:26 -0500

    initial release

--- action.js
+++ action.js
@@ -2,19 +2,7 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const process = require('process')
 
-const customRepo = repoPath => {
-  const segments = repoPath.split('/', 2)
-
-  if (segments.length < 2) {
-    core.setFailed("Please provide a `repo_path` in the format `owner/repo_name`")
-  }
-
-  return segments
-}
-
-const [owner, repo] = core.getInput('repo_path')
-  ? customRepo(core.getInput('repo_path'))
-  : process.env['GITHUB_REPOSITORY'].split('/', 2)
+const [owner, repo] = process.env['GITHUB_REPOSITORY'].split('/', 2)
 
 const octokit = new github.GitHub(
   core.getInput('github_token', { required: true })
@@ -23,36 +11,37 @@ const octokit = new github.GitHub(
 async function run() {
   let latestRelease
 
+  core.info(`Fetching the latest release for \`${owner}/${repo}\``)
+
   try {
     latestRelease = await octokit.repos.getLatestRelease({
       owner,
       repo,
     })
   } catch (error) {
+    core.info('Could not fetch the latest release. Have you made one yet?')
     core.setFailed(error)
   }
 
-  const latestReleaseTag = latestRelease.tagName
+  const { data } = latestRelease
 
-  core.info(`Fetched latest release tag: ${latestReleaseTag}`)
-
-  core.setOutput('url', latestRelease.url)
-  core.setOutput('assets_url', latestRelease.assetsUrl)
-  core.setOutput('upload_url', latestRelease.uploadUrl)
-  core.setOutput('html_url', latestRelease.htmlUrl)
-  core.setOutput('id', latestRelease.id)
-  core.setOutput('node_id', latestRelease.nodeId)
-  core.setOutput('tag_name', latestReleaseTag)
-  core.setOutput('target_commitish', latestRelease.targetCommitish)
-  core.setOutput('name', latestRelease.name)
-  core.setOutput('draft', latestRelease.draft)
-  core.setOutput('author_id', latestRelease.author.id)
-  core.setOutput('author_node_id', latestRelease.author.nodeId)
-  core.setOutput('author_url', latestRelease.author.url)
-  core.setOutput('author_login', latestRelease.author.login)
-  core.setOutput('author_html_url', latestRelease.author.htmlUrl)
-  core.setOutput('author_type', latestRelease.author.type)
-  core.setOutput('author_site_admin', latestRelease.author.siteAdmin)
+  core.setOutput('url', data.url)
+  core.setOutput('assets_url', data.assets_url)
+  core.setOutput('upload_url', data.upload_url)
+  core.setOutput('html_url', data.html_url)
+  core.setOutput('id', data.id.toString())
+  core.setOutput('node_id', data.node_id)
+  core.setOutput('tag_name', data.tag_name)
+  core.setOutput('target_commitish', data.target_commitish)
+  core.setOutput('name', data.name)
+  core.setOutput('draft', data.draft)
+  core.setOutput('author_id', data.author.id.toString())
+  core.setOutput('author_node_id', data.author.node_id)
+  core.setOutput('author_url', data.author.url)
+  core.setOutput('author_login', data.author.login)
+  core.setOutput('author_html_url', data.author.html_url)
+  core.setOutput('author_type', data.author.type)
+  core.setOutput('author_site_admin', data.author.site_admin)
 }
 
 try {
@@ -60,4 +49,3 @@ try {
 } catch (error) {
   core.setFailed(`Action failed with error ${error}`)
 }
-
